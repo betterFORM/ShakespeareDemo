b2c0a1ae256c22502beb2f8f5e2616a38f8a0fc1
Lars Windauer <lars.windauer@betterform.de>
2014-04-04 17:34:01 +0200

    * first version of the TEI monitor (recognizes if user clicks on a tei tagged element)

--- resources/scripts/annotation.js
+++ resources/scripts/annotation.js
@@ -5,17 +5,17 @@ var Annotations = Annotations || {};
  */
 Annotations.namespace = function (ns_string) {
     var parts = ns_string.split('.'), parent = Annotations, i;
-    if (parts[0] == "Annotations") {
-        parts = parts.slice(1);
-    }
-    for (i = 0; i < parts.length; i++) {
-        // create a property if it doesn't exist
-        if (typeof parent[parts[i]] == "undefined") {
-            parent[parts[i]] = {};
-        }
-        parent = parent[parts[i]];
-    }
-    return parent;
+	if (parts[0] == "Annotations") {
+		parts = parts.slice(1);
+	}
+	for (i = 0; i < parts.length; i++) {
+		// create a property if it doesn't exist
+		if (typeof parent[parts[i]] == "undefined") {
+			parent[parts[i]] = {};
+		}
+		parent = parent[parts[i]];
+	}
+	return parent;
 };
 
 Annotations.namespace("Annotations.Selection");
@@ -25,25 +25,67 @@ Annotations.Selection = (function () {
         var self = this;
 
         // setup TEI Editor
-        $("#saveTeiAnnotation").on("click", function(){
+        $("#saveTeiAnnotation").on("click", function(){ 
             console.log("calling this.saveCreateAnnotation()");
             self.saveCreateAnnotation();
         });
         $("#cancelTeiAnnotation").on("click", function(){ $("#teiEditor").hide(); });
-
+        
         // setup mouse listener 
-        console.log("cssMatcher to add mouselistener to: ", cssMatcher);
-        this.setupMouseListener(cssMatcher);
+        console.log("cssMatcher to add mouselistener to: ", cssMatcher)
+        this.setupMouseListener(cssMatcher); 
     };
-
-
+    
+    
     /*#
-     # Function to handle users text selection
-     #*/
+      # Function to handle users text selection
+      #*/
     Constr.prototype.setupMouseListener = function(cssMatcher) {
-        // console.log("setup Mouselistener");
+        console.log("setup Mouselistener");
         var self = this;
-        $( cssMatcher ).mouseup(function() {
+        $(cssMatcher).mousedown(function() {
+            console.log("mousedown")
+            $("#selectionMonitor").text("").html();
+        });
+        $(cssMatcher).click(function() {
+            var selection = rangy.getSelection();
+            var text = selection.toString();
+            var html = selection.toHtml();
+            var surroundingNode = $(selection.anchorNode.parentNode);
+            console.log(surroundingNode.text());
+            if(surroundingNode.hasClass("rs")) {
+                $("#selectionMonitor").text("rs: " + surroundingNode.text());
+            }else if(surroundingNode.hasClass("emph")) {
+                $("#selectionMonitor").text("emph: "+ surroundingNode.text());
+            }else if(surroundingNode.hasClass("name")) {
+                $("#selectionMonitor").text("name: "+ surroundingNode.text());
+            }
+            else {
+                $("#selectionMonitor").text();
+            }
+          
+        
+        });
+        $(cssMatcher).dblclick(function() {
+            var selection = rangy.getSelection();
+            var text = selection.toString();
+            var html = selection.toHtml();
+            console.log("dbclick: ", html);
+            $("#selectionMonitor").text(html).html();
+        
+        });
+        $(cssMatcher).mousemove(function() {
+            var selection = rangy.getSelection();
+            var text = selection.toString();
+            if(text != ""){
+                var html = selection.toHtml();
+                console.log(html);
+                $("#selectionMonitor").text(html).html();
+            }else {
+                console.log("empty selection")
+            }
+        });
+        /** $(cssMatcher).mouseup(function() {
             $("#teiEditor").hide();
             var selection = rangy.getSelection();
             var text = selection.toString();
@@ -54,11 +96,11 @@ Annotations.Selection = (function () {
             var focusOffset = selection.focusOffset;
             console.log("selection: ", selection);
             console.log("selection.isBackwards: ", selection.isBackwards());
-            // console.log("selection.inspect(): ", selection.inspect());
-            // console.log("selection.getName(): ", selection.getName());
-            // console.log("selection.nativeSelection: ", selection.nativeSelection);
-            // self.printProperties(selection);
-
+            console.log("selection.inspect(): ", selection.inspect());
+            console.log("selection.getName(): ", selection.getName());
+            console.log("selection.nativeSelection: ", selection.nativeSelection);
+             
+            self.printProperties(selection); 
             // Case 1: Nothing was selected
             if(text === "" && html === ""){
                 console.log("no Text Selected");
@@ -66,7 +108,7 @@ Annotations.Selection = (function () {
             // 
             else if(anchorNode.isEqualNode(focusNode)){
                 console.log("selected Text is within the same node. TEI node xml:id: ", $(anchorNode.parentNode).attr('xml:id'));
-
+                
                 if($(anchorNode.parentNode).text() == text){
                     console.log("whole TEI node was selected");
                     // open surrounding TEI Node in Editor
@@ -77,21 +119,21 @@ Annotations.Selection = (function () {
                 }
             }
             else if(anchorOffset == 0 && focusOffset == 0){
-                console.log("focus and anchor offset are 0");
+                console.log("focus and anchor offset are 0")
                 self.showTeiEditor(anchorNode.parentNode);
                 self.showTeiEditor(anchorNode.parentNode)
             }else if($(html).text() == $(focusNode).text()){
                 console.log("sel.html.text() equals focusNode.text()");
                 self.showTeiEditor(anchorNode.parentNode)
             }
-
+            
             else {
                 console.log("parent nodes of selection are different");
                 self.printProperties(selection);
             }
-        });
-    };
-
+        });  */
+    }
+    
     Constr.prototype.saveCreateAnnotation = function() {
         console.log("saveCreateAnnotation");
         //do your own request an handle the results
@@ -104,11 +146,11 @@ Annotations.Selection = (function () {
                 return false;
             }
         });
-    };
-
+    }
+    
     /*#
-     # Position and show the TEI Editor
-     #*/
+      # Position and show the TEI Editor
+      #*/
     Constr.prototype.showTeiEditor = function(node) {
         var teiNode = $(node);
         var teiEditor= $("#teiEditor");
@@ -121,22 +163,22 @@ Annotations.Selection = (function () {
         $("#teiTypeSelector").val(teiType);
         $("#teiTypeValue").val(teiNode.text());
         $("#teiXmlId").val(xmlId);
-
+        
         // position the tei editor
         var teiPos = teiNode.position();
         console.log("teiPos:", teiPos);
         var leftPos = teiPos.left + teiNode.width();
         var topPos = teiPos.top + teiNode.height();
-
+        
         teiEditor.css({left:leftPos,top:topPos});
         teiEditor.show();
 
     };
-
-
+    
+    
     /*#
-     # Function to print relevant rangy attributes to the js console
-     #*/
+      # Function to print relevant rangy attributes to the js console
+      #*/
     Constr.prototype.printProperties = function(selection) {
         var text = selection.toString();
         var html = selection.toHtml();
@@ -153,7 +195,7 @@ Annotations.Selection = (function () {
         console.log("sel.anchorNode.parentNode:", anchorNode.parentNode);
         var pointsAreEqual = rangy.dom.comparePoints(anchorNode, anchorOffset, focusNode, focusOffset);
         console.log("pointsAreEqual: ",pointsAreEqual);
-    };
-
+    }
+    
     return Constr;
 }());
